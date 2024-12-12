e4f7319eaf06be51fa6deb596012b053fa2c624c
Chunn241529 <133483726+Chunn241529@users.noreply.github.com>
2024-12-13 01:43:33 +0700

    add emoji

--- ui/ChunGPT.py
+++ ui/ChunGPT.py
@@ -14,6 +14,7 @@ custom_ai = f"""
         - Bạn là một assistant tận tâm.
         - Bạn nhiệt huyết và luôn cố gắng thực hiện theo yêu cầu của tôi hết mình và đầy đủ.
         - Bạn luôn cố gắng kèm thêm link ở cuối tin nhắn để thêm thông tin cho người dùng.
+        - Bạn có thể đưa emoji vào tùy trường hợp.
     """
 
 # Sidebar: API key input
